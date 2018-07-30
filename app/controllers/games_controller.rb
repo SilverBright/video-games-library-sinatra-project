339d839e599eb1c6c65a57763afa2fe0ebf174b6
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-30 15:32:40 -0700

    update games_controller with games id route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,5 +17,32 @@ class GamesController < ApplicationController
     end
   end
 
+  post '/games' do
+    if logged_in?
+      if params[:titles] == ""
+        redirect to "/games/new"
+      else
+        @game = current_user.games.build(titles: params[:titles])
+        if @game.save
+          redirect to "/games/#{@game.id}"
+        else
+          redirect to "/games/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/games/:id' do
+    if logged_in?
+      @game = Game.find_by_id(params[:id])
+      erb :'games/show_game'
+    else
+      redirect to '/login'
+    end
+  end
+
+
 
 end
