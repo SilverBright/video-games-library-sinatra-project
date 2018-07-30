ff00fee8007ae0cb450bf7d5dd2fd9bdcd79d1ad
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-30 15:00:01 -0700

    update games_controller with first get command

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,12 @@
 class GamesController < ApplicationController
 
+  get '/games' do
+    if logged_in?
+      @games = Game.all
+      erb :'games/games'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,21 @@ class UsersController < ApplicationController
     end
   end
 
-
   get '/login' do
     if !logged_in?
       erb :'users/login'
     else
       redirect to '/games'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/games"
+    else
+      redirect to '/signup'
     end
   end
 
