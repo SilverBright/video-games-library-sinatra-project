e95e50b3df93ddc3623425f23e7ab7b705863624
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-30 14:08:47 -0700

    update index page layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,23 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+      erb :'users/create_user', locals: {message: "Please create a new account."}
     else
       redirect to '/games'
     end
   end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/games'
+    end
+  end
+
 
   get '/login' do
     if !logged_in?
