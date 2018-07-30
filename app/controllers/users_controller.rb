89da3221bcf5231c91240f5fc57ddf2a55c91134
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-30 13:45:22 -0700

    add CRUD actions to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/games'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/games'
+    end
+  end
+
 end
 
