f0d9c8415837afadfdba7587b3b861670098b6c8
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-31 16:23:50 -0700

    add slug method to controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,39 @@
 class UsersController < ApplicationController
 
   get '/register' do
-    if !logged_in?
-      erb :'users/register', locals: {message: "Please create a new account."}
+    if logged_in?
+      redirect "/users/#{@user.slug}"
+      # erb :'users/register'
     else
-      redirect to '/games'
+      erb :'/users/register'
     end
   end
 
   post '/register' do
-    if params[:username] == "" || params[:password] == ""
-      redirect to '/register'
-    else
-      @user = User.new(:username => params[:username], :password => params[:password])
-      @user.save
+   @user = User.create(username: params[:username], password: params[:password])
+    if @user.valid?
       session[:user_id] = @user.id
-      redirect to '/games'
+      redirect "/users/#{@user.slug}"
+    elsif @user.invalid? && User.find_by(username: @user.username)
+       flash[:error] = "That username is already taken"
+      redirect '/register'
+    else
+        flash[:error] = "You must fill out all fields to sign up."
+        redirect '/register'
     end
   end
+
+
+  # post '/register' do
+  #   if params[:username] == "" || params[:password] == ""
+  #     redirect to '/register'
+  #   else
+  #     @user = User.new(:username => params[:username], :password => params[:password])
+  #     @user.save
+  #     session[:user_id] = @user.id
+  #     redirect to '/games'
+  #   end
+  # end
 
   get '/login' do
     if !logged_in?
@@ -28,21 +44,31 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/games"
-    else
-      redirect to '/login'
+   @user = User.find_by(:username => params[:username])
+   if @user && @user.authenticate(params[:password])
+
+     session[:user_id] = @user.id
+
+     flash[:success] = "Welcome Back, #{@user.username}!"
+     redirect "/users/#{@user.slug}"
+
+   else
+     flash[:error] = "Invalid username or password. Please try again."
+     redirect '/login'
+   end
+ end
+
+ get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @games = @user.games
+      erb :'/users/show'
+
     end
-  end
 
   get '/logout' do
     if logged_in?
-      session.destroy
+      session.clear
       redirect to '/login'
-    else
-      redirect to '/'
     end
   end
 
