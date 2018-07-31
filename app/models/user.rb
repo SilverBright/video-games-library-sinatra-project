138737634745a82ab7d42a8cb9433f83bc539819
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-31 13:27:04 -0700

    add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :games
+
+	validates :username, presence: true, uniqueness: true
+  	validates :email, presence: true, uniqueness: true
+  	
 	has_secure_password
 end
