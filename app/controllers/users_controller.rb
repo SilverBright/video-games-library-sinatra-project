a8d310498c739f7866ee8e50487caf8d8d934a91
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-07-31 22:08:27 -0700

    deleted users_show page due to redundancy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,18 +34,19 @@ class UsersController < ApplicationController
    @user = User.find_by(:username => params[:username])
    if @user && @user.authenticate(params[:password])
      session[:user_id] = @user.id
-     redirect to "/games"
+     redirect to "/users/show"
    else
      flash[:error] = "Invalid username or password. Please try again."
-     redirect '/login'
+     redirect '/games'
    end
  end
 
   get '/logout' do
     if logged_in?
       session.clear
-      redirect to '/'
+      redirect to '/login'
     end
   end
+
 end
 
