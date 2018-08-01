d232e1f1b6819d07bdf8b118523b6c928209a2b5
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-08-01 10:17:16 -0700

    add slug method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ActiveRecord::Base
 	has_many :games
 	validates :username, presence: true, uniqueness: true
 	has_secure_password
+
+
+	def slug
+		username.downcase.gsub(" ", "-")
+	end
+
+	def self.find_by_slug(slug)
+		User.all.find{|user| user.slug == slug}
+	end
+
+
 end
